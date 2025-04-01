ecd732850f30efcc4286fd5818af4c44c356422f
Fazel montazery <ryan.montazery@protonmail.com>
2025-04-01 23:22:24 +0330

    Set WindowHints and fullscreen mode with f key

--- src/defs.h
+++ src/defs.h
@@ -5,4 +5,7 @@
 		.window	= NULL,		\
 		.winWidth = 1000,	\
 		.winHeight = 1000,	\
+		.winPosX = 0,		\
+		.winPosY = 0,		\
+		.fullscreen = false,	\
 	}
--- src/main.c
+++ src/main.c
@@ -14,6 +14,9 @@ struct
 	GLFWwindow* window;
 	int32_t winWidth;
 	int32_t winHeight;
+	int32_t winPosX;
+	int32_t winPosY;
+	bool fullscreen;
 } state = DEFAULT_STATE;
 
 // Blueprints
@@ -21,6 +24,7 @@ static bool initWindow(int32_t width, int32_t height);
 static void loop(void);
 static void deinitApp(void);
 
+// Main
 int main(void)
 {
 	if(!initWindow(state.winWidth, state.winHeight)) return -1;
@@ -40,14 +44,47 @@ static void glfw_error_callback(int error, const char* description)
 static void glfw_framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+	if (!state.fullscreen) {
+		state.winWidth = width;
+		state.winHeight = height;
+	}
+}
+
+void glfwWindowPosCallback(GLFWwindow* window, int xpos, int ypos) 
+{
+	if (!state.fullscreen) {
+		state.winPosX = xpos;
+		state.winPosY = ypos;
+	}
 }
 
 static void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	else if (key == GLFW_KEY_Q && action == GLFW_PRESS)
+	}
+	else if (key == GLFW_KEY_Q && action == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+	else if (key == GLFW_KEY_F && action == GLFW_PRESS) {
+		GLFWmonitor* primary = glfwGetPrimaryMonitor();
+		if (!primary) return;
+		const GLFWvidmode* mode = glfwGetVideoMode(primary);
+		if (!mode) return;
+		if (state.fullscreen) {
+			glfwSetWindowMonitor(state.window, NULL, state.winPosX, state.winPosY,
+					state.winWidth,
+					state.winHeight,
+					0);
+			state.fullscreen = false;
+		} else {
+			glfwSetWindowMonitor(state.window, primary, 0, 0,
+					mode->width,
+					mode->height,
+					mode->refreshRate);
+			state.fullscreen = true;
+		}
+	}
 }
 
 // Initialize the window for opengl
@@ -60,20 +97,49 @@ static bool initWindow(int32_t width, int32_t height)
 	if (!glfwInit())
 		return false;
 
+#ifdef NDEBUG
+	glfwWindowHint(GLFW_CONTEXT_DEBUG, GLFW_TRUE);
+#endif
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 #endif
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+	glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+	glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
+	glfwWindowHint(GLFW_FLOATING, GLFW_FALSE);
+	glfwWindowHint(GLFW_CENTER_CURSOR, GLFW_TRUE);
+	glfwWindowHint(GLFW_SAMPLES, 0);
+	glfwWindowHint(GLFW_REFRESH_RATE, GLFW_DONT_CARE);
+	glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+	glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
+	
 
 	state.window = glfwCreateWindow(width, height, "FreeVisualizer", NULL, NULL);
 	if (!state.window)
 		return false;
 
+	GLFWmonitor* primary = glfwGetPrimaryMonitor();
+	if (!primary)
+		return false;
+
+	const GLFWvidmode* mode = glfwGetVideoMode(primary);
+	if (mode) {
+		int xpos = (mode->width - state.winWidth) / 2;
+		int ypos = (mode->height - state.winHeight) / 2;
+		glfwSetWindowPos(state.window, xpos, ypos);
+        }
+
 	glfwMakeContextCurrent(state.window);
 	glfwSetFramebufferSizeCallback(state.window, glfw_framebuffer_size_callback);
+	glfwSetWindowPosCallback(state.window, glfwWindowPosCallback);
 	glfwSetKeyCallback(state.window, glfw_key_callback);
+
+	// setting v-sync
+	glfwSwapInterval(1);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
@@ -84,6 +150,7 @@ static bool initWindow(int32_t width, int32_t height)
 	return true;
 }
 
+// Main drawing loop
 static void loop(void)
 {
 	while (!glfwWindowShouldClose(state.window))
